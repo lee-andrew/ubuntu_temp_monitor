fce312de3effb8c8094c13e64dff08c0ed2b6cc4
lee-andrew <lee.andrew076@gmail.com>
2017-12-16 01:12:33 -0500

    changed task.sh to handle dynamic files

--- tasks/status_threshold/task.sh
+++ tasks/status_threshold/task.sh
@@ -2,24 +2,34 @@
 set -e
 
 pushd states
-  result=$(cat $readings)
+  for i in *; do
+    eval "$i=\$(cat $i)"
+  done
+  states=$(ls)
 popd
 pushd git_monitor/state
-  if [ $result -gt $threshold ]; then
-    if [ ! -s $readings  ]; then
-      echo $result > $readings
+  for i in ${states[*]}; do
+    readings=$i
+    a="${i}_threshold"
+    threshold=${!a}
+    a="${i}"
+    result=${!a}
+    if [ $result -gt $threshold ]; then
+      if [ ! -s $readings  ]; then
+        echo $result > $readings
+      fi
+    else
+      if [ -s $readings ]; then
+        echo "" > $readings
+      fi
     fi
-  else
-    if [ -s $readings ]; then
-      echo "" > $readings
+    if [[ -n $(git status --porcelain) ]]; then
+      git config --global user.name $git_user
+      git config --global user.email $git_email
+      git add .
+      git commit -m "state change $readings"
     fi
-  fi
-  if [[ -n $(git status --porcelain) ]]; then
-    git config --global user.name $git_user
-    git config --global user.email $git_email
-    git add .
-    git commit -m "state change $readings"
-  fi
+  done
 popd
 shopt -s dotglob
 cp -r git_monitor/* output
