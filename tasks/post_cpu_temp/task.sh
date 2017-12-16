abe00fb63086b5fb2fc562868b4ad3e562ded6d5
lee-andrew <lee.andrew076@gmail.com>
2017-12-16 14:43:41 -0500

    added cpu_temp tasks

---
+++ tasks/post_cpu_temp/task.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+cd git_monitor_trigger_cpu_temp/state
+  a=$(cat $bot_type)
+  if [ "$a" == "" ]; then
+    message="$bot_type has gone below the threshold"
+  else
+    message="$bot_type has gone above the threshold at $a"
+  fi
+cd ..
+curl $discord_webhook -d "{\"content\":\"$message\"}"
